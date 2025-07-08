10e0b529a72a278ca1cf370a25c63a386aec182d
Omar Khaled <45782982+omar39@users.noreply.github.com>
2025-07-08 18:10:13 +0300

    Update YDKReader.py
    making regex more precise

--- YDKReader.py
+++ YDKReader.py
@@ -10,14 +10,21 @@ class Reader:
         self.deck.pop(card_id)
 
     def _process_file(self, file_name: str):
+        ydk_deck = []
+        card_id_regex = re.compile(r"^\d+$")
         try:
             with open(file_name, "r") as f:
-                ydk_deck = re.findall('\\d+', ''.join(f.readlines()))
+                for line in f:
+                    # Remove leading/trailing whitespace, including newlines
+                    stripped_line = line.strip()
+                    # Check if the stripped line matches the card ID regex
+                    if card_id_regex.match(stripped_line):
+                        ydk_deck.append(stripped_line)
         except FileNotFoundError:
             print(f"File {file_name} not found.")
             return None
 
-        deck = dict()
+        deck = {}
 
         for card in ydk_deck:
             if deck.get(card) == None:
